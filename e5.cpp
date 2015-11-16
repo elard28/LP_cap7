84fb42890fd8cd7e33a4402c79240b2c9296d387
Elard Guevara <elard.guevara@ucsp.edu.pe>
2015-11-16 14:27:37 -0500

    avance

---
+++ e5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int fn(int n)
+{
+	n=n+5;
+	return 5;
+}
+
+int main(int argc, char const *argv[])
+{
+	int a=2;
+	int b=2;
+
+	int x=a+fn(a);
+	int y=fn(b)+b;
+
+	return 0;
+}
--- e6.cs
+++ e6.cs
@@ -2,8 +2,21 @@ using System;
 
 public class e6
 {
+	static int fn(ref int n)
+	{
+		n=n+5;
+		return 5;
+	}
+
 	public static void Main()
 	{
-		// your code goes here
+		int a=2;
+		int b=2;
+
+		int x=a+fn(ref a);
+		int y=fn(ref b)+b;
+
+		Console.WriteLine("x: "+x);
+		Console.WriteLine("y: "+y);
 	}
 }
